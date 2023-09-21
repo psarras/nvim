480b395f994d3ac05afd0a138152ac1bcea0f7d2
spsarras <psarras.st@gmail.com>
2023-09-21 19:49:12 +0100

    theme

--- lua/stam/init.lua
+++ lua/stam/init.lua
@@ -1,4 +1,5 @@
 require("stam.remap")
 print("from stam")
 require("stam.packer")
-
+require("catppuccin").setup({flavour="mocha"})
+vim.cmd.colorscheme "catppuccin"
--- lua/stam/remap.lua
+++ lua/stam/remap.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set('t', 'jk', [[<C-\><C-n>]]) -- no need to escape the '\'
 vim.keymap.set('i', 'jk', [[<C-\><C-n>]]) -- no need to escape the '\'
+
