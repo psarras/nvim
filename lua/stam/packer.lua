453934725f75b420f5d6edca216b24443ed61ea0
spsarras <psarras.st@gmail.com>
2023-09-21 20:30:24 +0100

    treesitter, requires config on windows as well though

--- lua/stam/packer.lua
+++ lua/stam/packer.lua
@@ -12,4 +12,11 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
   use { "catppuccin/nvim", as = "catppuccin" }
+  use {
+	  'nvim-treesitter/nvim-treesitter',
+	  run = function()
+		  local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+		  ts_update()
+	  end,
+  }
 end)
