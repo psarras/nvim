0a6ba68e163c64096a1f9a1feaa677771651032c
stam <stam@mx-laptop>
2023-09-22 22:46:49 +0100

    some shortcuts

--- lua/stam/init.lua
+++ lua/stam/init.lua
@@ -1,5 +1,6 @@
 require("stam.remap")
-print("from stam")
+--print("from stam")
 require("stam.packer")
+require("stam.set")
 require("catppuccin").setup({flavour="mocha", transparent_background = false})
 vim.cmd.colorscheme "catppuccin"
--- lua/stam/remap.lua
+++ lua/stam/remap.lua
@@ -3,3 +3,38 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set('t', 'jk', [[<C-\><C-n>]]) -- no need to escape the '\'
 vim.keymap.set('i', 'jk', [[<C-\><C-n>]]) -- no need to escape the '\'
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
