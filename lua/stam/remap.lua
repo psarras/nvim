ad4b8075689631c2b6e47716d5d3aea5e92bf29b
spsarras <psarras.st@gmail.com>
2023-10-04 14:17:30 +0100

    minor navigation maps

--- lua/stam/remap.lua
+++ lua/stam/remap.lua
@@ -3,38 +3,15 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set('t', 'jk', [[<C-\><C-n>]]) -- no need to escape the '\'
 vim.keymap.set('i', 'jk', [[<C-\><C-n>]]) -- no need to escape the '\'
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "H", "^")
+vim.keymap.set("n", "L", "%")
 
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+vimkeymap.set("n", "<leader>wa", ":w")
 
