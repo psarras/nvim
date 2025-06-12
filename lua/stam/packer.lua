f7545237c9aac437b70c8180c8f3e85e4496ca93
spsarras <psarras.st@gmail.com>
2025-06-12 11:50:28 +0100

    pinned all plugin versions

--- lua/stam/packer.lua
+++ lua/stam/packer.lua
@@ -5,26 +5,27 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+    use {'wbthomason/packer.nvim', commit = 'ea0cc3c59f67c440c5ff0bbe4fb9420f4350b9a3' }
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.3',
         -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+        requires = { { 'nvim-lua/plenary.nvim', commit = '857c5ac632080dba10aae49dba902ce3abf91b35' } }
     }
-    use { "catppuccin/nvim", as = "catppuccin" }
+    use { "catppuccin/nvim", as = "catppuccin", commit = 'fa42eb5e26819ef58884257d5ae95dd0552b9a66' }
     use {
         'nvim-treesitter/nvim-treesitter',
+        commit = '42fc28ba918343ebfd5565147a42a26580579482',
         run = function()
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
         end,
     }
-    use {'akinsho/git-conflict.nvim', tag = "*", config = function()
+    use {'akinsho/git-conflict.nvim', commit = '4bbfdd92d547d2862a75b4e80afaf30e73f7bbb4', config = function()
       require('git-conflict').setup()
     end}
-    use { "theprimeagen/harpoon" }
-    use { "mbbill/undotree" }
-    use { "tpope/vim-fugitive" }
+    use { "theprimeagen/harpoon", commit = '1bc17e3e42ea3c46b33c0bbad6a880792692a1b3' }
+    use { "mbbill/undotree", commit = 'b951b87b46c34356d44aa71886aecf9dd7f5788a'}
+    use { "tpope/vim-fugitive", commit = '4a745ea72fa93bb15dd077109afbb3d1809383f2' }
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
@@ -41,16 +42,16 @@ return require('packer').startup(function(use)
             { 'L3MON4D3/LuaSnip' },
         }
     }
-    use { 'tpope/vim-surround' }
+    use { 'tpope/vim-surround', commit = '3d188ed2113431cf8dac77be61b842acb64433d9' }
     use {
-        'numToStr/Comment.nvim',
+        'numToStr/Comment.nvim', commit = 'e30b7f2008e52442154b66f7c519bfd2f1e32acb',
         config = function()
             require('Comment').setup()
         end
     }
-    use { 'github/copilot.vim' }
+    use { 'github/copilot.vim', commit = '3955014c503b0cd7b30bc56c86c56c0736ca0951' }
     use({
-      "kylechui/nvim-surround",
+      "kylechui/nvim-surround", commit = '8dd9150ca7eae5683660ea20cec86edcd5ca4046',
       config = function()
         require("nvim-surround").setup({})
       end
