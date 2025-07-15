786bb17ebecf107a600bff884e0df303a1c0a56e
spsarras <psarras.st@gmail.com>
2025-07-15 12:59:11 +0100

    added terminal

--- lua/stam/init.lua
+++ lua/stam/init.lua
@@ -3,6 +3,7 @@ require("stam.remap")
 require("stam.set")
 require("catppuccin").setup({ flavour = "mocha", transparent_background = false })
 vim.cmd.colorscheme "catppuccin"
+require("stam.terminal")
 
 -- briefly highlight selection
 vim.api.nvim_create_autocmd("TextYankPost", {
@@ -63,3 +64,6 @@ require('refactoring').setup({
     show_success_message = true, -- shows a message with information about the refactor on success
                                   -- i.e. [Refactor] Inlined 3 variable occurrences
 })
+
+
+
