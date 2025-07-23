d47f872464f685d35466057093065f3cabc4f71c
spsarras <psarras.st@gmail.com>
2025-07-23 12:00:46 +0100

    show recent buffers with fr

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,7 @@ local builtin = require('telescope.builtin')
 
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fr', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fc', function()
 	builtin.grep_string({search = vim.fn.input("Grep > ")});
