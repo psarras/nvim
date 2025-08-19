bb04e53fe7ae41cff66066b5c5d7af7c624e7d61
spsarras <psarras.st@gmail.com>
2025-08-19 15:36:51 +0100

    changed terminal shortcut to tw

--- lua/stam/terminal.lua
+++ lua/stam/terminal.lua
@@ -236,7 +236,7 @@ local function CloseFloatingTerminal()
 end
 
 -- Key mappings
-vim.keymap.set("n", "<leader>t", FloatingTerminal, { noremap = true, silent = true, desc = "Toggle floating terminal" })
+vim.keymap.set("n", "<leader>tw", FloatingTerminal, { noremap = true, silent = true, desc = "Toggle floating terminal" })
 vim.keymap.set({ "t", "n" }, "<Esc>", function()
     if terminal_state.is_open then
         vim.api.nvim_win_close(terminal_state.win, false)
