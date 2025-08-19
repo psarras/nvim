46be8cef93d850059c852e9778c567f21715323e
spsarras <psarras.st@gmail.com>
2025-08-19 15:41:15 +0100

    updated to use telescope for grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,6 +4,10 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fr', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fc', function()
-	builtin.grep_string({search = vim.fn.input("Grep > ")});
-end)
+
+-- vim.keymap.set('n', '<leader>fc', function()
+-- 	builtin.grep_string({search = vim.fn.input("Grep > ")});
+-- end)
+
+
+vim.keymap.set('n', '<leader>fc', builtin.live_grep, {desc = "Live grep"});
